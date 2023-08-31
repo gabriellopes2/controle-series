6c6429abb7b4e2e6d9d5b4ed43ce9fa31dbd27ec
Gabriel <gabriel.lopes2@universo.univates.br>
2023-08-31 12:00:03 -0300

    Criação da model série.

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,18 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Serie;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
-        $series = [
-            'Rain',
-            'TWD',
-            'Supernatural',
-            "Gray's Anatomy"
-        ];
+        $series = Serie::all();
 
         return view('series.index')->with('series', $series);
     }
@@ -21,5 +18,14 @@ class SeriesController extends Controller
     public function create(Request $request)
     {
         return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        $serie = new Serie();
+        $serie->nome = $request->input('nome');
+        $serie->save();
+
+        return redirect('/series');
     }
 }
